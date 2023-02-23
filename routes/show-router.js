b9e68d48f1349fb0ce05932a2531dd09891afe91
= <=>
2023-02-23 23:35:28 +0000

    user done

--- routes/show-router.js
+++ routes/show-router.js
@@ -1,38 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const { Show } = require("../models/Show");
+const { sequelize } = require("sequelize");
 
 // GET all shows
-router.get("/show", async (request, response) => {
-  const respond = await Show.findall(Show);
+router.get("/", async (request, response) => {
+  const respond = await Show.findAll();
   response.send(respond);
 });
 // GET one show
-router.get("/show/:id", async (request, response) => {
+router.get("/:id", async (request, response) => {
   const respond = await Show.findByPk(request.params.id);
   response.send(respond);
 });
 // GET shows of a particular genre (genre in req.params)
-router.get("/show/:id", async (request, response) => {
-  const respond = await Show.findByPk(request.params.id);
-  response.send(respond);
+router.get("/:genre", async (request, response) => {
+  const findgenre = request.params.genre;
+  const shows = await Show.findAll({
+    where: { genre: { [Op.iLike]: findgenre } },
+  });
+  response.send(shows);
 });
+
 // PUT update rating of a show that has been watched
-router.put("/show/:id", async (request, response) => {
+router.put("/:id/rating", async (request, response) => {
   const respond = await Show.update(request.body.update, {
     where: request.body.where,
   });
   response.send(respond);
 });
 // PUT update the status of a show
-router.put("/show/:id", async (request, response) => {
-  const respond = await Show.update(request.body.update, {
-    where: request.body.where,
-  });
-  response.send(respond);
+router.put("/:id", async (request, response) => {
+  const { id } = request.params;
+  const { status } = request.body;
+
+  const result = await Show.update({ status }, { where: { id } });
+
+  response.send(result);
 });
+
 // DELETE a show
-app.delete("/show/:id", async (request, response) => {
+router.delete("/:id/delete", async (request, response) => {
   const respond = await Show.destroy({
     where: { id: request.params.id },
   });
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,28 +1,36 @@
 const express = require("express");
+const { Show } = require("../models");
 const router = express.Router();
 const { User } = require("../models/User");
 
 //GET all users
-router.get("/user", async (request, response) => {
-  const respond = await User.findall(User);
+router.get("/", async (request, response) => {
+  const respond = await User.findAll();
   response.send(respond);
 });
 //GET one user
-router.get("/user/:id", async (request, response) => {
+router.get("/:id", async (request, response) => {
   const respond = await User.findByPk(request.params.id);
   response.send(respond);
 });
-//GET al shows watched
-router.get("/user/:id", async (request, response) => {
-  const respond = await User.findByPk(request.params.id);
-  response.send(respond);
+//GET all shows watched
+router.get("/:id/watched", async (request, response) => {
+  const user = await User.findByPk(request.params.id);
+  const show = await user.getShows();
+  response.send(show);
 });
 //Update user shows if watched
-router.put("/user/:id", async (request, response) => {
-  const respond = await User.update(request.body.update, {
-    where: request.body.where,
+router.put("/:id/show/:showid", async (request, response) => {
+  const user = await User.findByPk(request.params.id);
+  const show = await Show.findByPk(request.params.showid);
+  user.addShow(show);
+  const updatedUser = await User.findByPk(request.params.id, {
+    include: {
+      model: Show,
+    },
   });
-  response.send(respond);
+
+  response.send(updatedUser);
 });
 
 module.exports = router;
